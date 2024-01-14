4df7f3163f3476d101cf201866014dafedb06d1e
RishikesavanRamesh <rishikesavanramesh@proton.me>
2024-01-14 10:19:09 +0000

    Add /login route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,8 @@
-import {Router, Request, Response} from "express";
+import {Router, Request, Response, NextFunction} from "express";
 import bcrypt from "bcrypt";
+
+import jwt from "jsonwebtoken";
+
 
 import { UserModel } from "../models/user";
 
@@ -27,8 +30,51 @@ router.post("/register", async (req: Request, res: Response) =>  {
     catch (err) {
         res.status(500).json({type: err});
     }
-
-
 });
+
+router.post("/login", async (req: Request, res: Response) =>  {
+    const { username, password } = req.body;
+
+    
+    try {
+        const user = await UserModel.findOne({ username });
+
+    if (!user) {
+        return res.status(400).json({type: UserErrors.NO_USER_FOUND});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({type: UserErrors.WRONG_CREDENTIALS});  
+    }
+
+    const token = jwt.sign({id:user._id}, "secret");
+    res.json({token, userID: user._id});
+
+    }
+    catch (err) {
+        res.status(500).json({type: err});
+    }
+});
+
+export const verifyToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {    
+        const authHeader  = req.headers.authorization;
+        if (authHeader) {
+            jwt.verify(authHeader, "secret", (err) => {
+            if (err){
+                return res.sendStatus(403);
+            }
+            next();
+        });
+        }
+
+        return res.sendStatus(401);
+    }
+
 
 export { router as userRouter};
