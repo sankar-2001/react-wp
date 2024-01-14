a8a7d29eee603338fe3eba9629eedb9bfb7ea001
RishikesavanRamesh <rishikesavanramesh@proton.me>
2024-01-14 10:56:23 +0000

    Add basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
-    <div>
-     
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" />
+          <Route path="/auth" />
+          <Route path="/settings" />
+          <Route path="/dashboard" />
+        </Routes>
+      </Router>
     </div>
   );
 }
