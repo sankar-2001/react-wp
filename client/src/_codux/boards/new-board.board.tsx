8bb46f4351fff927d28e8dec3f4946e3b2aa2654
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-16 10:16:44 +0000

    Add component path

--- client/src/_codux/boards/new-board.board.tsx
+++ client/src/_codux/boards/new-board.board.tsx
@@ -1,11 +1,15 @@
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
-    name: 'New Board',
-    Board: () => <div></div>,
+    name: 'App2',
+    Board: () => <div className="App2">
+        <form>
+            <label>Name</label>
+            <input />
+        </form></div>,
     isSnippet: true,
     environmentProps: {
-        canvasWidth: 140,
-        canvasHeight: 90
+        canvasWidth: 422,
+        canvasHeight: 274
     }
 });
