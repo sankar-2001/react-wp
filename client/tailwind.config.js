fc054b7d055261493e83209938898241b9ec9894
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-17 07:09:22 +0000

    Fix export types

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
