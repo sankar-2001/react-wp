6de9fce34ad33166ed460faa544b5fd01c05b0e5
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-18 12:57:03 +0000

    Fix until Codux integration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,37 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export const darkMode = ["class"];
-export const content = [
-  './pages/**/*.{ts,tsx}',
-  './components/**/*.{ts,tsx}',
-  './app/**/*.{ts,tsx}',
-  './src/**/*.{ts,tsx}',
-];
-export const prefix = "";
-export const theme = {
-  container: {
-    center: true,
-    padding: "2rem",
-    screens: {
-      "2xl": "1400px",
-    },
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
   },
-  extend: {
-    keyframes: {
-      "accordion-down": {
-        from: { height: "0" },
-        to: { height: "var(--radix-accordion-content-height)" },
-      },
-      "accordion-up": {
-        from: { height: "var(--radix-accordion-content-height)" },
-        to: { height: "0" },
-      },
-    },
-    animation: {
-      "accordion-down": "accordion-down 0.2s ease-out",
-      "accordion-up": "accordion-up 0.2s ease-out",
-    },
-  },
-};
+  plugins: [],
+}
 
-// eslint-disable-next-line no-undef
-export const plugins = [require("tailwindcss-animate")];
