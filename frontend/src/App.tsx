b9d3a97435d84a073b2a497c644715d8ee31d961
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-18 23:42:11 +0000

    Add message box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,11 @@
 function App() {
   return (  
     <>
-<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-  <div className="shrink-0">
-    <img className="h-12 w-12" src="" alt="ChitChat Logo"/>
-  </div>
-  <div>
+<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 " >
+
+  <div className="text-4xl text-gray-600">
     <div className="text-xl font-medium text-black">ChitChat</div>
-    <p className="text-red-500">You have a new message!</p>
+    <p className="text-green-100 bg-orange-300 bg-gradient-to-r from-red-500 to-purple-600 p-4 items-center rounded-2xl drop-shadow-lg">You have a new message!</p>
   </div>
 </div>
     </>
