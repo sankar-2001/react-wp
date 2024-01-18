72e0f8da6854a70c9d13f9949fd8edf9c5fa64ee
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-18 12:45:08 +0000

    Add codux dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   </div>
   <div>
     <div className="text-xl font-medium text-black">ChitChat</div>
-    <p className="text-slate-500">You have a new message!</p>
+    <p className="text-red-500">You have a new message!</p>
   </div>
 </div>
     </>
