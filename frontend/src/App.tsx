0514c028098686005ae7a9662caeeb8f8f32d046
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-18 12:30:33 +0000

    Fix tailwindcss, shadcn/ui and vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  return (  
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div className="shrink-0">
+    <img className="h-12 w-12" src="" alt="ChitChat Logo"/>
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">ChitChat</div>
+    <p className="text-slate-500">You have a new message!</p>
+  </div>
+</div>
     </>
   )
 }
