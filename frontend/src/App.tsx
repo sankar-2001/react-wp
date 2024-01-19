82c2db03f4ea5f2a44c9e07520a7273ed23d6555
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-19 00:46:44 +0000

    Add dark mode with toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
-
-
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
 function App() {
   return (  
-    <>
-<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 " >
-
-  <div className="text-4xl text-gray-600">
-    <div className="text-xl font-medium text-black">ChitChat</div>
-    <p className="text-green-100 bg-orange-300 bg-gradient-to-r from-red-500 to-purple-600 p-4 items-center rounded-2xl drop-shadow-lg">You have a new message!</p>
+ 
+<ThemeProvider>
+  <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 " >
+  
+    <div className="text-4xl text-gray-600">
+      <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+      <p className="text-green-100 bg-orange-300 bg-gradient-to-r from-red-500 to-purple-600 p-4 items-center rounded-2xl drop-shadow-lg hover:text-green-400">You have a new message!</p>
+    </div>
+    <div>
+  
+      <div className="rounded-lg bg-red-400 py-1 px-2 m-2">Thello</div>
+      <div className="rounded-lg bg-red-400 py-1 px-2 m-2">Thello</div>
+      <div className="rounded-lg bg-red-400 py-1 px-2 m-2">Thello</div>
+      <ModeToggle/>
+    </div>
   </div>
-</div>
-    </>
+</ThemeProvider>
   )
 }
 
