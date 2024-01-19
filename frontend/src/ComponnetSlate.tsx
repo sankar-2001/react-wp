6d68f1eafdfe0a0a15858d3ae326bdcf126e2a76
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-19 16:12:09 +0000

    Add design

--- frontend/src/ComponnetSlate.tsx
+++ frontend/src/ComponnetSlate.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "@/components/theme-provider"
 // import { ModeToggle } from "@/components/mode-toggle"
-import {RosNode }  from "./components/rosnode"
 
 function ComponentSlate() {
   return (  
  
 <ThemeProvider>
-  <div >
-      <RosNode />
+  <div className="flex p-2 justify-between">
+    {/* search bar */}
+    <div className="flex bg-red-300 rounded-sm  mr-auto py-1 px-2">search bar</div>
+    <div className="flex bg-blue-300 rounded-sm ml-auto py-1 px-2">hello</div>
   </div>
 </ThemeProvider>
   )
