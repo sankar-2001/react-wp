b00f1a4c28eabf0dd1dfa39c2656d79dd4ca7411
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-20 02:55:29 +0000

    Add sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider"
 import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar"
+import MySidebar from "./scenes/global/Sidebar"
+import Dashboard from "./scenes/Dashboard";
+import AuthPage from "./scenes/AuthPage";
 
 
 function App() {
   return (  
  
 <ThemeProvider>
-      <Router>
         <Topbar/>
+        <MySidebar/>
+        <Dashboard/>  
+      <Router>
         <Routes>
           <Route path="/" element={<h1>empty is this</h1>}/>
-          {/* <Route path="/auth" element={<AuthPage/>} />
-          <Route path="/settings" element={<SettingPage/>}/>
-          <Route path="/dashboard" element={<DashboardPage/>}/> */}
+          <Route path="/auth" element={<AuthPage/>} />
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/dboard" element={<h1>this is business</h1>}/>
+          <Route path="/d2board" element={<h1>this is masdfass</h1>}/>
+
         </Routes>
       </Router>
 </ThemeProvider>
--- frontend/src/scenes/global/Sidebar.tsx
+++ frontend/src/scenes/global/Sidebar.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import ReceiptRoundedIcon from "@mui/icons-material/ReceiptRounded";
+import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
+import TimelineRoundedIcon from "@mui/icons-material/TimelineRounded";
+import BubbleChartRoundedIcon from "@mui/icons-material/BubbleChartRounded";
+import WalletRoundedIcon from "@mui/icons-material/WalletRounded";
+import AccountBalanceRoundedIcon from "@mui/icons-material/AccountBalanceRounded";
+import SavingsRoundedIcon from "@mui/icons-material/SavingsRounded";
+import MonetizationOnRoundedIcon from "@mui/icons-material/MonetizationOnRounded";
+import SettingsApplicationsRoundedIcon from "@mui/icons-material/SettingsApplicationsRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import ShieldRoundedIcon from "@mui/icons-material/ShieldRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import { Link } from "react-router-dom"
+import { Cable, HelpCircle } from "lucide-react";
 
-export default function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const MySidebar = () => {
+    return (
+        <div className="flex h-screen">
+          <Sidebar className="app">
+            <Menu>
+              <MenuItem className="menu1" icon={<MenuRoundedIcon />}>
+                <h2> ROBOTOAI</h2>
+              </MenuItem>
+              <MenuItem icon={<GridViewRoundedIcon />}> Dashboard </MenuItem>
+              <SubMenu label="Insights" icon={<BarChartRoundedIcon />}>
+                <MenuItem icon={<TimelineRoundedIcon />}> Timeline Chart </MenuItem>
+                <MenuItem icon={<BubbleChartRoundedIcon />}>Bubble Chart</MenuItem>
+              </SubMenu>
+              <MenuItem icon={<Cable />}>Teleop</MenuItem>
+              <MenuItem icon={<HelpCircle />}>FAQ</MenuItem>
+              <SubMenu label="Settings" icon={<SettingsApplicationsRoundedIcon />}>
+                <MenuItem icon={<AccountCircleRoundedIcon />}> Account </MenuItem>
+                <MenuItem icon={<ShieldRoundedIcon />}> Privacy </MenuItem>
+                <MenuItem icon={<NotificationsRoundedIcon />}>
+                  Notifications
+                </MenuItem>
+              </SubMenu>
+              <MenuItem icon={<LogoutRoundedIcon />}> Logout </MenuItem>
+            </Menu>
+          </Sidebar>
+        </div>
+    );
+  };
+  export default MySidebar;
