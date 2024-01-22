2515c9550bdde72bf31cfb732dc4ff1b179e2264
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-22 14:49:36 +0000

    Remove client folder

--- client/src/_codux/boards-global-setup.ts
+++
@@ -1,5 +0,0 @@
-// boards-global-setup.ts
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-
-import '../tailwind.css';
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (  
  
 <ThemeProvider>
-        <Topbar/>
+        <Topbar/>````               
         <h1>from app</h1>
        <Dashboard/>  
       <Router>
