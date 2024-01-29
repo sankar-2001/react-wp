ee170637ca4a1c3673ca31fd437efaac5261cb4c
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-29 01:56:05 +0000

    Refactor App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,48 @@
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-import { ThemeProvider } from "@/components/theme-provider"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
 import Topbar from "./scenes/global/Topbar";
-import MySidebar from "./scenes/global/Sidebar"
+import MySidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/Dashboard";
 import AuthPage from "./scenes/AuthPage";
 
-import {Register} from "./components/auth";
+import { Register } from "./components/auth";
 import { Login } from "./components/auth";
+import RobotCard from "./components/RobotCard";
 
 function App() {
-  return (  
- 
-<ThemeProvider>
+  return (
+    <ThemeProvider>
       <Router>
-        <Routes>
-          <Route path="/register" element={<AuthPage><Register/></AuthPage>} />
-          <Route path="/login" element={<AuthPage><Login/></AuthPage>} />
-          <Route path="/" element={<MySidebar/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-        </Routes>
+        <div className="flex relative">
+          <MySidebar />
+          <main>
+            <Topbar />
+            <RobotCard robot_id={"hello"} />
+            <RobotCard robot_id={"world"} />
+            <RobotCard robot_id={"simmon"} />
+            <Routes>
+              <Route
+                path="/register"
+                element={
+                  <AuthPage>
+                    <Register />
+                  </AuthPage>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <AuthPage>
+                    <Login />
+                  </AuthPage>
+                }
+              />
+            </Routes>
+          </main>
+        </div>
       </Router>
-</ThemeProvider>
-  ) 
+    </ThemeProvider>
+  );
 }
 
-
-export default App
+export default App;
