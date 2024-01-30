51eaf20edaa26a4d7071e60aa21a6f5e232bb121
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2024-01-30 02:16:01 +0000

    Add Stickyness

--- frontend/src/scenes/global/Topbar.tsx
+++ frontend/src/scenes/global/Topbar.tsx
@@ -7,15 +7,14 @@ import { Bell, BellDot } from "lucide-react"
 export default function Topbar() {
 
   return (
-    <div className="flex p-2 justify-between h-10">
+    <div className="flex p-2 justify-between sticky top-0 bg-red-200">
     {/* search bar */}
 
 
-    <Input className="flex-initial h-10 w-36 rounded-sm mr-auto py-1 px-2 " type="string" placeholder="Search" />
+    <Input className="flex-initial h-10 w-36 rounded-sm mr-auto py-1 px-2 bg-pink-200" type="string" placeholder="Search" />
 
-    <div className="flex h-10 rounded-sm ml-auto py-1 px-2">
+    <div className="flex h-10 rounded-sm ml-auto py-1 px-2 bg-purple-400">
       {/* <h1><ModeToggle/></h1> */}
-        <ModeToggle/>
         <BellDot/>
         <Bell/>
         <SettingsIcon />
